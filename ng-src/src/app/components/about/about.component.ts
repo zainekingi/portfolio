3799838fe3540f02b48b142aaf6114a8dadd5dd0
zaine-kingi <zainekingi@users.noreply.github.com>
2017-07-16 12:39:02 +1200

    add onscroll event method and implement on section and images.

--- ng-src/src/app/components/about/about.component.ts
+++ ng-src/src/app/components/about/about.component.ts
@@ -10,6 +10,32 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    window.onscroll = function() {
+
+      var c = document.querySelectorAll('.section, .skill-item');
+
+      // Loop through all the logos.
+      for (var i = 0; i < c.length; i++) {
+
+        // Hide all image elements.
+        c[i].classList.add('opacity-0', 'slide-down');
+
+        // Get the image elements offset top position.
+        var vpOffset = c[i].getBoundingClientRect();
+
+        // Check the parent containers offset top position.
+        if (vpOffset.top < 500) {
+
+          // Show the element
+          c[i].classList.remove('opacity-0', 'slide-down');
+
+          // Fade element in.
+          c[i].classList.add('fade-in', 'slide-up');
+        }
+      }
+    }; /* END: window onscroll method */
+
   }
 
 }
