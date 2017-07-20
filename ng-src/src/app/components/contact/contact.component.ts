03aa4253a8e4966fd478254a71832bb8ccd78d26
zaine-kingi <zainekingi@users.noreply.github.com>
2017-07-20 18:55:58 +1200

    add public height and viewport height variables.

--- ng-src/src/app/components/contact/contact.component.ts
+++ ng-src/src/app/components/contact/contact.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  public height: Number;
+
   constructor() { }
 
   ngOnInit() {
+    var vpH = window.innerHeight;
+    this.height = vpH;
   }
 
 }
