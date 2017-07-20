6b2bdc011a9e692a39a8735ffabe378b7bb5856e
zaine-kingi <zainekingi@users.noreply.github.com>
2017-07-20 18:47:22 +1200

    removed the onScroll method and window onscroll event.

--- ng-src/src/app/components/about/about.component.ts
+++ ng-src/src/app/components/about/about.component.ts
@@ -9,50 +9,6 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-
-    var s = false;
-
-    window.onscroll = onScroll;
-
-      function onScroll() {
-
-      s = true;
-
-      // Get the clients window height.
-      var wnH = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-
-
-      var c = document.querySelectorAll('.section, .company-logo');
-
-      // Loop through all the logos.
-      for (var i = 0; i < c.length; i++) {
-
-        // Hide all image elements.
-        c[i].classList.add('opacity-0', 'slide-down');
-
-        // Get the image elements offset top position.
-        var vpOffset = c[i].getBoundingClientRect();
-
-        // Check the parent containers offset top position.
-        if (vpOffset.top < wnH) {
-
-          // Show the element
-          c[i].classList.remove('opacity-0', 'slide-down');
-
-          // Fade element in.
-          c[i].classList.add('fade-in', 'slide-up');
-        }
-      }
-    } /* END: onScroll method */
-
-    //  Set onScroll to fire every 1000ms.
-    setInterval(function() {
-      if(s) {
-        s = false;
-      }
-    }, 1000);
-
-  }
+  ngOnInit() {}
 
 }
