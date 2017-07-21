758bf6a44cab6e68d326985a12353575c4f2f5c1
zaines-s9 <rous6026nz@gmail.com>
2017-07-21 18:16:46 +1200

    change timeout value to 15ms to improve performance.

--- ng-src/src/app/components/navbar/navbar.component.ts
+++ ng-src/src/app/components/navbar/navbar.component.ts
@@ -38,7 +38,7 @@ export class NavbarComponent implements OnInit {
               clearInterval(openFn);
             }
           }
-        }, .1);
+        }, 15);
         isOpen = true;
       } else {
         var closeFn = setInterval(function() {
@@ -55,7 +55,7 @@ export class NavbarComponent implements OnInit {
               clearInterval(closeFn);
             }
           }
-        }, .1);
+        }, 15);
         isOpen = false;
       }
     }
