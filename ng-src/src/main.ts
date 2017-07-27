afeb3c20cd138afb065eda442c97245b29f9d665
zaine-kingi <zainekingi@users.noreply.github.com>
2017-07-27 18:32:02 +1200

    add if statement to enable prodMode if on live server.

--- ng-src/src/main.ts
+++ ng-src/src/main.ts
@@ -1,9 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
-if (environment.production) {
+// Enable production mode unless running locally
+if (!/localhost/.test(document.location.host)) {
   enableProdMode();
 }
 
