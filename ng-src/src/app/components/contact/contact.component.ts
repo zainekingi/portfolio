1991a20da5ebc8111332983e719ea153df11474c
zaine-kingi <zainekingi@users.noreply.github.com>
2017-07-30 23:46:27 +1200

    implement contact form funtionality and styling.

--- ng-src/src/app/components/contact/contact.component.ts
+++ ng-src/src/app/components/contact/contact.component.ts
@@ -1,19 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
 export class ContactComponent implements OnInit {
 
-  public height: Number;
+  private contactForm: FormGroup;
+  private name: FormControl;
+  private email: FormControl;
+  private message: FormControl;
 
-  constructor() { }
+  constructor() {
+
+  }
+
 
   ngOnInit() {
-    var vpH = window.innerHeight;
-    this.height = vpH;
+
+    this.createFormControls();
+    this.createForm();
+
+  }
+
+  /* Create new form controls */
+
+  createFormControls() {
+    this.name = new FormControl('', Validators.required);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ]);
+    this.message = new FormControl('', Validators.required);
+  }
+
+
+  /* Create new form instance */
+
+  createForm() {
+    this.contactForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+    });
+  }
+
+  /* Form submit check */
+
+  checkForm() {
+
+    if(this.contactForm.valid) {
+      console.log("Form submitted");
+      this.contactForm.reset();
+    } else {
+      console.log("Not valid?");
+      this.contactForm.reset();
+    }
+
   }
 
 }
