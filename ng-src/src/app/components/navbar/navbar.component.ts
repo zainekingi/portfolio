cacac898f22abe3efea8444409706d5ef075d7e0
zaine-kingi <zainekingi@users.noreply.github.com>
2017-07-30 23:49:45 +1200

    refine navbar styling and improve toggle animation and positioning.

--- ng-src/src/app/components/navbar/navbar.component.ts
+++ ng-src/src/app/components/navbar/navbar.component.ts
@@ -10,9 +10,10 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
     var ham = document.getElementsByClassName('ham')[0];
     var navLnks = document.getElementsByClassName('nav')[0];
-    var count = 75;
+    var count = -350;
     var isOpen;
     var vpWidth = window.innerWidth;
 
@@ -22,17 +23,19 @@ export class NavbarComponent implements OnInit {
 
     ham.addEventListener('click', toggleNav);
 
+    /* Method to control the navigation toggling. */
+
     function toggleNav(){
       if(!isOpen) {
         var openFn = setInterval(function() {
-          if(vpWidth > 768) {
-            if(count !== 200) {
+          if(vpWidth > 768) { // desktop
+            if(count < 70) {
               openNavDesk(navLnks);
             } else {
               clearInterval(openFn);
             }
           } else {
-            if(count !== 200) {
+            if(count < 55) {  // mobile
               openNavMob(navLnks);
             } else {
               clearInterval(openFn);
@@ -42,14 +45,14 @@ export class NavbarComponent implements OnInit {
         isOpen = true;
       } else {
         var closeFn = setInterval(function() {
-          if(vpWidth > 768) {
-            if(count > 75) {
+          if(vpWidth > 768) { // desktop
+            if(count > -300) {
               closeNavDesk(navLnks);
             } else {
               clearInterval(closeFn);
             }
           } else {
-            if(count > 50) {
+            if(count > -300) {  // mobile
               closeNavMob(navLnks);
             } else {
               clearInterval(closeFn);
@@ -61,26 +64,59 @@ export class NavbarComponent implements OnInit {
     }
 
 
+    /* Method to close the navigation if a navigaion link is click. */
+
+    var lis = document.querySelectorAll('.nav-items li');
+    for(var i = 0; i < lis.length; i ++) {
+
+        lis[i].addEventListener('click', function(){
+
+          var closeNavFn = setInterval(function() {
+            if(vpWidth > 768) { // desktop
+              if(count > -300) {
+                closeNavDesk(navLnks);
+              } else {
+                clearInterval(closeNavFn);
+              }
+            } else {
+              if(count > -300) {  // mobile
+                closeNavMob(navLnks);
+              } else {
+                clearInterval(closeNavFn);
+              }
+            }
+          }, 1);
+          isOpen = false;
+        });
+    }
+
+
+    /* Helper functions */
+
     function openNavDesk(ele) {
-      count ++;
+      count += 15;
+      ele.style.left = count + 'px';
       ele.style.height = '100%';
-      ele.style.width = count + 'px';
+      ele.style.width = '300px';
+      ele.style.top = '0px';
     }
 
     function closeNavDesk(ele) {
-      count --;
-      ele.style.width = count + 'px';
+      count -= 15;
+      ele.style.left = count + 'px';
     }
 
     function openNavMob(ele) {
-      count ++;
-      ele.style.height = count + 'px';
+      count += 15;
+      ele.style.top = count + 'px';
       ele.style.width = '100%';
+      ele.style.height = '300px';
+      ele.style.left = '0px';
     }
 
     function closeNavMob(ele) {
-      count --;
-      ele.style.height = count + 'px';
+      count -= 15;
+      ele.style.top = count + 'px';
     }
   }
 
