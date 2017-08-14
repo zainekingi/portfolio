6098de872e8a49c8ee67a24068fc5f84d4e3007f
zaine-kingi <zainekingi@users.noreply.github.com>
2017-08-14 22:16:54 +1200

    production build V30.

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,104 @@
+/* ================================================
+*   FILE:   MAIN.JS
+*   AUTHOR: Zaine Kingi
+*   VER:    1.0
+*   DATE:   23-7-17
+*   DESC:   Main JavaScript file for reusable
+*           JavaScript methods for the application.
+*           .......................................
+*                       CHANGE LOG
+*           ---------------------------------------
+*           VER:    DATE:   CHANGES:
+*           ---------------------------------------
+*           1.0     23-7-17 Initial release.
+*  ----------------------------------------------- */
+
+/* ------------------------------------------------
+    METHOD: SLIDER
+    DESC:   Image slider method.
+    INPUT:  ID of parent container of the images.
+    OUTPUT: NULL
+   --------------------------------------------- */
+
+function initSlider(eleId) {
+
+  var ele;
+  var par;
+  var imgs;
+  var imageWidth;
+  var imageNumber;
+  var currentImage = 0;
+
+  function init(){
+
+    ele = document.getElementById(eleId);
+    imgs = ele.children;
+    imageNumber = imgs.length;
+    imageWidth = imgs[0].getBoundingClientRect().width;
+    // set ele width as the total height of all images in image slider.
+    ele.style.width = parseInt(imageWidth * imageNumber + imageWidth) + 'px';
+    slider(ele);
+  }
+
+  function slider(e){
+
+    animate({
+      delay:200,
+      duration: 5000,
+      delta:function(p){return Math.max(0, -1 + 2 * p)},
+      step:function(delta){
+        e.style.left = '-' + parseInt(currentImage * imageWidth + delta * imageWidth) + 'px';
+      },
+      callback:function(){
+        currentImage++;
+        // if it doesn’t slide to the last image, keep sliding
+        if(currentImage < imageNumber-1){
+          slider(e);
+        }
+        // if current image it’s the last one, it slides back to the first one
+        else{
+          var leftPosition = (imageNumber - 1) * imageWidth;
+          // after 2 seconds, call the goBack function to slide to the first image
+          setTimeout(function(){goBack(leftPosition)},2000);
+          setTimeout(function(){slider(e)}, 5000);
+        }
+      }
+    });
+  }
+
+  function goBack(leftPosition){
+
+    currentImage = 0;
+    var id = setInterval(function(){
+      if(leftPosition >= 0){
+        ele.style.left = '-' + parseInt(leftPosition) + 'px';
+        leftPosition -= imageWidth / 10;
+      }
+      else{
+        clearInterval(id);
+      }
+    }, 17);
+  }
+
+  function animate(opts){
+
+    var start = new Date;
+    var id = setInterval(function(){
+      var timePassed = new Date - start;
+      var progress = timePassed / opts.duration;
+      if(progress > 1){
+        progress = 1;
+      }
+      var delta = opts.delta(progress);
+      opts.step(delta);
+      if (progress == 1){
+        clearInterval(id);
+        opts.callback();
+      }
+    }, opts.dalay || 20);
+  }
+  window.onload = init;
+}
+
+initSlider("slider");
+/* =============== END: Slider Method ============ */
